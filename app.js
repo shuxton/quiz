b5981161864df1b8d324985077654c2a72c5d8d7
Shubham N J <37372508+shubham-nj@users.noreply.github.com>
2020-05-21 20:17:25 +0530

    sanks ui

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ mongoose.connect(
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+//app.use(express.static(__dirname + "/public"));
+app.use('/public', express.static('public'));
 app.use(methodOverride("_method"));
 app.use(cookieParser("secret"));
 
