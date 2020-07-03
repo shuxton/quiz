82849b9c6ada74364d2a683dcf4648ce7022bb5c
shuxton <shubhamsnj@gmail.com>
2020-07-03 19:15:27 +0530

    bug fixes

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.use("/", indexRoutes);
 app.use("/quiz", quizRoutes);
 
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
-  console.log("shuxton's server has started!");
+  console.log("Shuxton's Server Has Started!");
 });
 
